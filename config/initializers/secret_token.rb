2063908e7af58fd7d81f01b12aae1703e41e08fe
Amruta Takawale <amruta.takawale@yahoo.in>
2015-02-13 14:38:26 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroblogApplication::Application.config.secret_token = '89db09bb7ed444a9fd9677a3231a12849918e4dcef248e6ee43febc0adf392e723098ec383f3b3542be7e950786a97c50576b3d605b27e53ea51527cc9c20126'
